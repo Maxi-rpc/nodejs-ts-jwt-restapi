4416e72d2fa1b17d3736712b701644a0942fae5e
Maximiliano <maxirpc2607@gmail.com>
2021-12-24 12:12:42 -0300

    test token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
 
+// jsonwebtoken
+import jwt from "jsonwebtoken";
+
 export const signUp = (req: Request, res: Response) => {
-	res.send("signup");
+	// user fake
+	const userFake = {
+		_id: 1,
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+	// token
+	const token: string = jwt.sign(
+		{
+			_id: userFake._id,
+		},
+		process.env.TOKEN_SECRET || "tokentest"
+	);
+
+	res.json(token);
+	// res.send("respuesta signup");
 };
 
 export const signIn = (req: Request, res: Response) => {
-	res.send("signin");
+	res.send("respuesta signin");
 };
 
 export const profile = (req: Request, res: Response) => {
-	res.send("profile");
+	res.send("respuesta profile");
 };
